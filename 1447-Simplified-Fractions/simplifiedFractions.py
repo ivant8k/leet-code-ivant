a0d9adc92e6061e7cc78d5f9ec6497caeb843e44
vant8k <163630054+ivant8k@users.noreply.github.com>
2025-07-01 18:57:51 +0700

    commonFactors

--- 1447-Simplified-Fractions/simplifiedFractions.py
+++ 1447-Simplified-Fractions/simplifiedFractions.py
@@ -12,16 +12,16 @@ class Solution(object):
         result = []
         for denominator in range(2, n + 1):
             for numerator in range(1, denominator):
-                print(f"Checking fraction {numerator}/{denominator}...")
-                print(f"GCD of {numerator} and {denominator} is {gcd(numerator, denominator)}")
+                # print(f"Checking fraction {numerator}/{denominator}...")
+                # print(f"GCD of {numerator} and {denominator} is {gcd(numerator, denominator)}")
                 if gcd(numerator, denominator) == 1:
                     # result.append(f"{numerator}/{denominator}")
                     result.append(str(numerator) + '/' + str(denominator))
         return result
     
 # Example usage:
-solution = Solution()
-print(solution.simplifiedFractions(2))
-print(solution.simplifiedFractions(3))
-print(solution.simplifiedFractions(4))
+# solution = Solution()
+# print(solution.simplifiedFractions(2))
+# print(solution.simplifiedFractions(3))
+# print(solution.simplifiedFractions(4))
 
---
+++ 2427-Number-of-Common-Factors/commonFactors.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def commonFactors(self, a, b):
+        """
+        :type a: int
+        :type b: int
+        :rtype: int
+        """
+        # result = []
+        n = 0
+        for i in range(1, min(a, b) + 1):
+            if a % i == 0 and b % i == 0:
+                # result.append(i)
+                n+= 1
+        return n
+    
+solution = Solution()
+print(solution.commonFactors(12, 6))
+print(solution.commonFactors(25, 30))
